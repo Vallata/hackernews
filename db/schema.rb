27fe78f33d210d65a4c4bfc2c5c700058e92e139
Alizee Vallat <alizee.v@live.fr>
2025-10-02 17:56:14 +0200

    create model votes direct on master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_02_132631) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_02_155442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,4 +36,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_02_132631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "posts"
+  add_foreign_key "votes", "users"
 end
